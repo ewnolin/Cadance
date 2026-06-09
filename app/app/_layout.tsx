bd67a16c47b7ebc3cd6e47487fd1cb11fb5653b8
William <olinwilliam7@gmail.com>
2026-06-09 19:25:18 +0200

    Add Train Next recommendations screen

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -36,6 +36,7 @@ function RootNavigator() {
       <Stack.Protected guard={!!user}>
         <Stack.Screen name="(tabs)" />
         <Stack.Screen name="session" options={{ presentation: "modal" }} />
+        <Stack.Screen name="recommendations" options={{ presentation: "modal" }} />
       </Stack.Protected>
       <Stack.Protected guard={!user}>
         <Stack.Screen name="sign-in" />
